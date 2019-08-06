338121fd623f00fc08fd9625bc2156543dfc4c35
Donald Yeh <Donald.Yeh@gmail.com>
2019-08-05 22:05:17 -0400

    creates targets component

---
+++ CRUD-targets-app/src/app/target.ts
@@ -0,0 +1,14 @@
+export class Target {
+  acquisitionStatus: string;
+  industry: string;
+  companyName: string;
+  companyAddress: string;
+  companySize: number;
+  ceo: string;
+  cfo: string;
+  cto: string;
+  liquidity: number;
+  profitability: number;
+  growth: number;
+  valuation: number;
+}
