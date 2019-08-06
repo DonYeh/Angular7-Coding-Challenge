9ae7302f93eb11ef174eb55aed2098065464fd6f
Donald Yeh <Donald.Yeh@gmail.com>
2019-08-06 15:59:17 -0400

    adds error handling

--- CRUD-targets-app/src/app/target.service.ts
+++ CRUD-targets-app/src/app/target.service.ts
@@ -4,6 +4,7 @@ import { Target } from "./target";
 import { TARGETS } from "./mock-company-data";
 import { MessageService } from "./message.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
@@ -28,7 +29,10 @@ export class TargetService {
 
   // gets targets from the server
   getTargets(): Observable<Target[]> {
-    return this.http.get<Target[]>(this.targetsUrl);
+    return this.http.get<Target[]>(this.targetsUrl).pipe(
+      tap(_ => this.log("fetched targets")),
+      catchError(this.handleError<Target[]>("getTargets", []))
+    );
   }
 
   getTarget(id: number): Observable<Target> {
@@ -40,5 +44,24 @@ export class TargetService {
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`TargetService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * param operation - name of the operation that failed
+   * param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
