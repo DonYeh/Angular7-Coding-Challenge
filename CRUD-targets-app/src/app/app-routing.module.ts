fa824cf2b9cd7706ba81f05d02a97210b6c60366
Donald Yeh <Donald.Yeh@gmail.com>
2019-08-06 13:00:59 -0400

    adds routing and navigation link to targets

--- CRUD-targets-app/src/app/app-routing.module.ts
+++ CRUD-targets-app/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { TargetsComponent } from "./targets/targets.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "targets", component: TargetsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
