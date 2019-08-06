1b4bc5b70cc6339fce376c40b139c24efab9df66
Donald Yeh <Donald.Yeh@gmail.com>
2019-08-05 22:14:33 -0400

    adds targets component to app template

--- CRUD-targets-app/src/app/targets/targets.component.ts
+++ CRUD-targets-app/src/app/targets/targets.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { TARGETS } from "../mock-company-data";
 @Component({
-  selector: 'app-targets',
-  templateUrl: './targets.component.html',
-  styleUrls: ['./targets.component.scss']
+  selector: "app-targets",
+  templateUrl: "./targets.component.html",
+  styleUrls: ["./targets.component.scss"]
 })
 export class TargetsComponent implements OnInit {
+  targets = TARGETS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
